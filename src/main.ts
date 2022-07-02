d630ff2be08949587fe0a44051d0130ba787121a
Frank Leonardo Puma Chara <frank@letsdobetter.world>
2022-07-02 14:07:27 -0500

    seryup project

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import {Request, Response} from 'express'
+
+const app = express()
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('hello world')
+})
+
+app.listen(3000, () => {
+    console.log('server running: http://localhost:3000')
+})
+
